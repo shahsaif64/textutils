4a1dd14861da355c268c8481b4cde414a46878c1
sharukh <csgosaifi5@gmail.com>
2023-07-15 14:43:11 +0530

    react project save1

---
+++ src/App.js
@@ -0,0 +1,23 @@
+
+import './App.css';
+import Navbar from './components/Navbar';
+import TextForm from './components/TextForm';
+
+
+
+function App() {
+  return (
+    <>
+     <Navbar title="TextUtils"/>
+     <TextForm text="Enter the text to analyze"/>
+
+    
+
+
+
+    </>
+    
+  );
+}
+
+export default App;
